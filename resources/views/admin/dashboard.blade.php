62c882e2fffba6ddf95568df40dd1adc6973e0d5
vishvakarmaditya <adityav2316672@gmail.com>
2025-03-19 15:09:42 +0530

    Added member login feature

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -69,18 +69,45 @@
                                     <div class="d-flex align-items-center">
                                         <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">{{$staff}}</span>
                                         <span class="badge badge-light-danger fs-base">
-                                            {{-- <i class="ki-duotone ki-arrow-down fs-5 text-danger ms-n1">
-                                                <span class="path1"></span>
-                                                <span class="path2"></span>
-                                            </i>2.2% --}}
+                                           
                                         </span>
                                     </div>
-                                    <span class="text-gray-400 pt-1 fw-semibold fs-6">Total Staff</span>
+                                    <span class="text-gray-400 pt-1 fw-semibold fs-6">Total Members</span>
                                 </div>
                             </div>
                         </div>
                     </div> 
-
+                     <div class="col-md-4">
+                        <div class="card card-flush text-dark">
+                            <div class="card-header pt-5">
+                                <div class="card-title d-flex flex-column">
+                                    <div class="d-flex align-items-center">
+                                        <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">{{$staff}}</span>
+                                        <span class="badge badge-light-danger fs-base">
+                                           
+                                        </span>
+                                    </div>
+                                    <span class="text-gray-400 pt-1 fw-semibold fs-6">Donations Received</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                     <div class="col-md-4">
+                        <div class="card card-flush text-dark">
+                            <div class="card-header pt-5">
+                                <div class="card-title d-flex flex-column">
+                                    <div class="d-flex align-items-center">
+                                        <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">{{$staff}}</span>
+                                        <span class="badge badge-light-danger fs-base">
+                                           
+                                        </span>
+                                    </div>
+                                    <span class="text-gray-400 pt-1 fw-semibold fs-6">Event Registrations</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                   
 
                     {{-- <div class="col-md-4">
                         <div class="card card-flush bg-warning text-dark">
